3d8a78f60495aaf5ff6fad49dc71156ef390e9ae
Jeong-Yoon-Hwan <wjddbsghks5742@gmail.com>
2022-12-01 15:36:38 +0900

    post 데이터 저장

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { execFile } = require("child_process");
 const fs = require("fs");
 const http = require('http'); 
 const { json } = require("stream/consumers");
+const qs = require('querystring')
 
 function fileRead(filename){
   const text = fs.readFileSync(`${filename}.txt`,'utf-8');
@@ -14,52 +15,31 @@ str += fileRead('header');
 str += fileRead('main');
 str += fileRead('footer');
 
-const arr = ['head','header','main','footer']
-function test(name){
-  let str =`<!DOCTYPE html>`
-  name.forEach(file => {
-    str += fileRead(`${file}`)
-  });
-}
-
-
-
-fs.writeFile('./index.txt',str,err=>{
-  if(err){
-    console.log(err)
-    return;
-  }
-})
-
 function router(url){
-  let name = url
   let text = fs.readFileSync(`${url}.txt`,'utf-8')
-  
   return text
 }
-
 
 const server = http.createServer(function(request,response){ 
   let{method} = request
   if(method === "GET"){ 
     let url = request.url
-    if(url !== '/favicon.ico'){
+    if(url !== '/favicon.ico' && url !== "/"){
       console.log(url)
       response.writeHead(200)
       response.write(str + router(url.substring(1)),'utf-8')
       response.end()
     }
   }else if(method === 'POST'){
+   
     request.on('data',function(data){
-      const textData = {
-        str : data.toString(),
-        time : new Date()
-      }
-      const dataJSON = JSON.stringify(textData)
+      let text = qs.parse(data.toString())
+      text.time = new Date();
+      const dataJSON = JSON.stringify(text)
       fs.writeFileSync('test-json.json',dataJSON)
     })
-    
   }
+
 
   response.writeHead(200,{'Content-Type':'text/html'});
   fs.readFile(__dirname + "/index.txt",(err,data)=>{
@@ -69,7 +49,6 @@ const server = http.createServer(function(request,response){
     response.end(data,'utf-8');
   })
 });
-
 
 server.listen(8080, function(){ 
   console.log('Server is running...');
--- test.js
+++
@@ -1,4 +0,0 @@
-function test(){
-  return "113"
-}
-module.exports= test;
